b89866559310eccdf6b4507f81f6d3ffc2451571
Deeksha Mahara <deekshamahara95@gmail.com>
2026-02-06 22:07:08 +0530

    Time: 1 ms (57.86%) | Memory: 8.6 MB (65.69%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+       long long reverse = 0;  
+        int original = x;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+
+        return reverse == original;
+    }
+};
